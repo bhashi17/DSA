a3bc531fefb9c73dd0ef53102352169f6f16656d
Inesh_Bhashithya <162081724+bhashi17@users.noreply.github.com>
2026-01-24 16:28:06 +0530

    Add files via upload

---
+++ Submission5/src/submission5/OptimizedInsertionSort.java
@@ -0,0 +1,31 @@
+
+package submission5;
+
+public class OptimizedInsertionSort {
+
+    static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Shift elements greater than key to one position ahead
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9, 5, 1, 4, 3};
+
+        insertionSort(arr);
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
---
+++ Submission5/src/submission5/Submission5.java
@@ -0,0 +1,17 @@
+
+package submission5;
+
+/**
+ *
+ * @author inesh
+ */
+public class Submission5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
